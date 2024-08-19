cb09bf0238dd54fcdc242ecf1f01570f0877751f
quirinpa <quirinpa@tty.pt>
2024-08-19 09:58:34 +0100

    Duplicate support and iteration

--- include/qhash.h
+++ include/qhash.h
@@ -5,27 +5,32 @@
 #include <sys/types.h>
 
 #define SHASH_PUT(hd, key, data)	hash_put(hd, key, strlen(key), data)
-#define SHASH_GET(hd, key)		hash_get(hd, key, strlen(key))
+#define SHASH_GET(hd, key)		hash_sget(hd, key, strlen(key))
 #define SHASH_DEL(hd, key)		hash_del(hd, key, strlen(key))
 
-struct hash_cursor {
-	void *key;
-	size_t key_len;
-	void *data;
+enum qhash_flags {
+	QH_DUP = 2,
 };
 
-unsigned hash_cinit(const char *file, const char *database, int mode);
+struct hash_cursor {
+	int flags;
+	void *internal;
+};
+
+unsigned hash_cinit(const char *file, const char *database, int mode, int flags);
 unsigned hash_init();
 void hash_put(unsigned hd, void *key, size_t key_len, void *value);
 void hash_cput(unsigned hd, void *key, size_t key_len, void *value, size_t value_len);
 void *hash_get(unsigned hd, void *key, size_t key_len);
+void *hash_sget(unsigned hd, void *key, size_t key_len);
 ssize_t hash_cget(unsigned hd, void *value, void *key, size_t key_len);
 void hash_del(unsigned hd, void *key, size_t key_len);
+int hash_vdel(unsigned hd, void *key_data, size_t key_size, void *value_data, size_t value_size);
 void shash_table(unsigned hd, char *table[]);
-struct hash_cursor hash_iter_start(unsigned hd);
-void *hash_iter_get(struct hash_cursor *cur);
+struct hash_cursor hash_citer(unsigned hd, void *key, size_t key_len);
+struct hash_cursor hash_iter(unsigned hd);
 void hash_close(unsigned hd);
-void *hash_iter_cget(void *key, void *value, struct hash_cursor *cur);
+ssize_t hash_next(void *key, void *value, struct hash_cursor *cur);
 void hash_sync(unsigned hd);
 
 #endif
--- qhash.c
+++ qhash.c
@@ -13,6 +13,10 @@
 #define HASH_DBS_MAX (64 * 512)
 #define HASH_NOT_FOUND NULL
 
+enum qhash_priv_flags {
+	QH_NOT_FIRST = 1,
+};
+
 DB *hash_dbs[HASH_DBS_MAX];
 struct free_id {
 	unsigned hd;
@@ -27,7 +31,7 @@ int hash_first = 1;
 void *txnid = NULL;
 
 unsigned
-hash_cinit(const char *file, const char *database, int mode)
+hash_cinit(const char *file, const char *database, int mode, int flags)
 {
 	DB **db;
 	struct free_id *new_id = NULL;
@@ -46,8 +50,16 @@ hash_cinit(const char *file, const char *database, int mode)
 
 	/* fprintf(stderr, "qhash new! %d\n", id); */
 	db = &hash_dbs[id];
-	if (db_create(db, NULL, 0) || (*db)->open(*db, txnid, file, database, DB_HASH, DB_CREATE, mode))
+	if (db_create(db, NULL, 0))
+		err(1, "hash_init: db_create");
+
+	if (flags & QH_DUP)
+		if ((*db)->set_flags(*db, DB_DUPSORT))
+			err(1, "hash_init: set_flags");
+
+	if ((*db)->open(*db, txnid, file, database, DB_HASH, DB_CREATE, mode))
 		err(1, "hash_init");
+
 	if (!new_id)
 		hash_n++;
 	return id;
@@ -56,7 +68,7 @@ hash_cinit(const char *file, const char *database, int mode)
 unsigned
 hash_init()
 {
-	return hash_cinit(NULL, NULL, 0644);
+	return hash_cinit(NULL, NULL, 0644, 0);
 }
 
 void
@@ -64,6 +76,7 @@ hash_cput(unsigned hd, void *key_r, size_t key_len, void *value, size_t value_le
 {
 	DB *db = hash_dbs[hd];
 	DBT key, data;
+	int ret;
 
 	memset(&key, 0, sizeof(DBT));
 	memset(&data, 0, sizeof(DBT));
@@ -72,8 +85,13 @@ hash_cput(unsigned hd, void *key_r, size_t key_len, void *value, size_t value_le
 	key.size = key_len;
 	data.data = value;
 	data.size = value_len;
+	int flags, dupes;
 
-	if (db->put(db, txnid, &key, &data, 0))
+	db->get_flags(db, &flags);
+	dupes = flags & DB_DUP;
+
+	ret = db->put(db, txnid, &key, &data, 0);
+	if (ret && (ret != DB_KEYEXIST || !dupes))
 		err(1, "hash_put");
 }
 
@@ -124,10 +142,20 @@ void *
 hash_get(unsigned hd, void *key_r, size_t key_len)
 {
 	size_t value_len;
-	void **ret = _hash_cget(hd, &value_len, key_r, key_len);
+	void *ret = _hash_cget(hd, &value_len, key_r, key_len);
 	if (!ret)
 		return NULL;
-	return *ret;
+	return ret;
+}
+
+void *
+hash_sget(unsigned hd, void *key_r, size_t key_len)
+{
+	void **ret = hash_get(hd, key_r, key_len);
+	if (ret)
+		return *ret;
+	else
+		return ret;
 }
 
 void
@@ -144,62 +172,104 @@ hash_del(unsigned hd, void *key_r, size_t len)
 		err(1, "hash_del");
 }
 
+int
+hash_vdel(unsigned hd, void *key_data, size_t key_size, void *value_data, size_t value_size)
+{
+	DB *db = hash_dbs[hd];
+	DBT key, data;
+	DBC *cursor;
+	int ret, flags = DB_SET;
+
+	if ((ret = db->cursor(db, txnid, &cursor, 0)) != 0) {
+		fprintf(stderr, "cursor: %s\n", db_strerror(ret));
+		return ret;
+	}
+
+	memset(&key, 0, sizeof(DBT));
+	key.data = key_data;
+	key.size = key_size;
+	memset(&data, 0, sizeof(DBT));
+	data.data = value_data;
+	data.size = value_size;
+
+	while (!(ret = cursor->get(cursor, &key, &data, flags))) {
+		flags = DB_NEXT_DUP;
+
+		if (data.size == value_size && memcmp(data.data, value_data, value_size) == 0) {
+			ret = cursor->del(cursor, 0);
+			break;
+		}
+	}
+
+	cursor->close(cursor);
+	return ret == DB_NOTFOUND ? 0 : ret;
+}
+
 void
 shash_table(unsigned hd, char *table[]) {
 	for (register char **t = table; *t; t++)
 		SHASH_PUT(hd, *t, *t + strlen(*t) + 1);
 }
 
-struct {
+struct hash_internal {
+	unsigned hd;
 	DB *db;
 	DBT data, key;
 	DBC *cursor;
-} c;
+};
 
 struct hash_cursor
-hash_iter_start(unsigned hd) {
+hash_citer(unsigned hd, void *key, size_t key_len) {
 	struct hash_cursor cur;
-	c.db = hash_dbs[hd];
-	c.db->cursor(c.db, NULL, &c.cursor, 0);
+	struct hash_internal *internal = malloc(sizeof(struct hash_internal));
+	cur.internal = internal;
+	internal->hd = hd;
+	internal->db = hash_dbs[hd];
+	internal->cursor = NULL;
+	internal->db->cursor(internal->db, NULL, &internal->cursor, 0);
+	memset(&internal->key, 0, sizeof(DBT));
+	memset(&internal->data, 0, sizeof(DBT));
+	internal->key.data = key;
+	internal->key.size = key_len;
+	cur.flags = 0;
+	if (key)
+		cur.flags = QH_DUP;
 	return cur;
 }
 
-void *
-hash_iter_get(struct hash_cursor *cur)
-{
-	int ret;
-	memset(&c.key, 0, sizeof(DBT));
-	memset(&c.data, 0, sizeof(DBT));
-
-	if ((ret = c.cursor->get(c.cursor, &c.key, &c.data, DB_NEXT))) {
-		if (ret != DB_NOTFOUND)
-			fprintf(stderr, "HASH_ITER: %s\n", db_strerror(ret));
-		c.cursor->close(c.cursor);
-		return NULL;
-	} else {
-		cur->key = c.key.data;
-		cur->key_len = c.key.size;
-		cur->data = * (void **) c.data.data;
-		return &c;
-	}
+struct hash_cursor
+hash_iter(unsigned hd) {
+	return hash_citer(hd, NULL, 0);
 }
 
-void *
-hash_iter_cget(void *key, void *value, struct hash_cursor *cur)
+ssize_t
+hash_next(void *key, void *value, struct hash_cursor *cur)
 {
-	int ret;
-	memset(&c.key, 0, sizeof(DBT));
-	memset(&c.data, 0, sizeof(DBT));
-
-	if ((ret = c.cursor->get(c.cursor, &c.key, &c.data, DB_NEXT))) {
+	struct hash_internal *internal = cur->internal;
+	ssize_t ret;
+	int flags = DB_FIRST;
+	if (cur->flags & QH_DUP) {
+		if (cur->flags & QH_NOT_FIRST)
+			flags = DB_NEXT_DUP;
+		else
+			flags = DB_SET;
+	} else if (cur->flags & QH_NOT_FIRST)
+		flags = DB_NEXT;
+	cur->flags |= QH_NOT_FIRST;
+	if ((ret = internal->cursor->get(internal->cursor, &internal->key, &internal->data, flags))) {
 		if (ret != DB_NOTFOUND)
-			fprintf(stderr, "HASH_ITER: %s\n", db_strerror(ret));
-		c.cursor->close(c.cursor);
-		return NULL;
+			fprintf(stderr, "hash_cnext: %u %d %s\n", internal->hd, cur->flags, db_strerror(ret));
+		internal->cursor->close(internal->cursor);
+		free(internal);
+		cur->internal = NULL;
+		return 0;
 	} else {
-		memcpy(key, c.key.data, c.key.size);
-		memcpy(value, c.data.data, c.data.size);
-		return &c;
+		ret = internal->key.size;
+		memcpy(key, internal->key.data, internal->key.size);
+		memcpy(value, internal->data.data, internal->data.size);
+		memset(&internal->key, 0, sizeof(DBT));
+		memset(&internal->data, 0, sizeof(DBT));
+		return ret;
 	}
 }
 
