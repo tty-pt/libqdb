0e079b5de8bc3a878445e35e868d32d0395ccafe
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-08-12 16:44:10 +0100

    Remove unnecessary things

--- include/qdb.h
+++ include/qdb.h
@@ -17,15 +17,6 @@
 #endif
 #include <qmap.h>
 
-enum qdb_type {
-	QDB_KEY = 0,
-	QDB_VALUE = 2,
-	QDB_REVERSE = 1,
-};
-
-extern qmap_type_t qdb_string, qdb_unsigned;
-extern unsigned types_hd, qdb_min;
-
 /* we have this config object mostly to avoid having
  * to specify much when opening databases */
 struct qdb_config {
@@ -37,20 +28,9 @@ struct qdb_config {
 extern struct qdb_config qdb_config;
 
 /* this is useful for custom logging */
+/* TODO move this to a logging library */
 typedef void (*log_t)(int type, const char *fmt, ...);
 void qdb_set_logger(log_t logger);
-extern log_t qdblog;
-
-static inline void qdblog_perror(char *str) {
-        qdblog(LOG_ERR, "%s: %s", str, strerror(errno));
-}
-
-__attribute__((noreturn))
-static inline void qdblog_err(char *str) {
-	qdblog(LOG_ERR, "%s\n", str);
-	raise(SIGTRAP);
-        exit(EXIT_FAILURE);
-}
 
 /* some flags that are useful for us */
 enum qdb_flags {
@@ -67,12 +47,6 @@ unsigned qdb_openc(const char *file, const char *database,
 		int mode, unsigned flags, int type,
 		char *key_tid, char *value_tid);
 
-/* get a key or value's length */
-size_t qdb_len(unsigned hd, unsigned type, void *key);
-
-/* get the type of a key or value */
-char *qdb_type(unsigned hd, unsigned type);
-
 /* initialize the system */
 void qdb_init(void);
 
@@ -81,16 +55,6 @@ void qdb_close(unsigned hd, unsigned flags);
 
 /* sync database to disk */
 void qdb_sync(unsigned hd);
-
-/* return our flags */
-unsigned qdb_flags(unsigned hd);
-
-/* the same but just give a length (for simple types) */
-void qdb_reg(char *key, size_t len);
-
-/* print a key or value */
-void qdb_print(char *target, unsigned hd,
-		unsigned type, void *thing);
 
 /* open a database (specify little) */
 static inline int
@@ -101,12 +65,6 @@ qdb_open(char *database, char *key_tid,
 			qdb_config.mode,
 			flags | qdb_config.flags,
 			qdb_config.type, key_tid, value_tid);
-}
-
-/* register a type of key / value */
-static inline void
-qdb_regc(char *key, qmap_type_t *type) {
-	qmap_put(types_hd, key, &type);
 }
 
 #endif
